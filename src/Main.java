cc56a96a8c88ebcad843b0fd22c192a554caf420
Douglas@PC Casa <douglasjordao@gmail.com>
2021-04-26 19:27:45 -0300

    Corrigindo espaçamento da mensagem de orientaçao

--- src/Main.java
+++ src/Main.java
@@ -419,7 +419,8 @@ public class Main {
       System.out.println("- " + Main.PNEUMONIA + ": " + mostraProbabilidadeDoanca(Main.PNEUMONIA_SINTOMAS, listaSintomas));
 
       System.out.println("\nProcure um médico informando o pré-diagnostico." 
-      + "\nLembre-se, o pré-diagnostico não exclui mudanças de possibilidades, pois os sintomas podem mudar e outros diagnósticos podem identificar novas doenças.");
+      + "\nLembre-se, o pré-diagnostico não exclui mudanças de possibilidades, pois os sintomas podem mudar e" 
+      + "\noutros diagnósticos podem identificar novas doenças.");
 
       System.out.println("\n");
     }
